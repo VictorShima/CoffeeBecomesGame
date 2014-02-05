06afe6f5b8a1b5a3e9ad2fd53237f6c25d8e4852
RafaelSchimassek <rafaelshima@live.com>
2014-02-05 19:05:49 +0000

    Adicionado o State do jogo Introduzido algumas funções da AI

--- src/java/com/md/mechevo/ai/AIAlgorithm.java
+++ src/java/com/md/mechevo/ai/AIAlgorithm.java
@@ -1,3 +1,32 @@
 package com.md.mechevo.ai;
 
-public final class AIAlgorithm {}
+
+/**
+ * AIAlgorithm will run its internal algorithm and pick an AIAction for the owner Solid to execute
+ */
+public final class AIAlgorithm {
+
+	private Player owner;
+
+
+	/**
+	 * Constructor with specified owner.
+	 * @param owner Owner of the AI
+	 */
+	public AIAlgorithm( Player owner ) {
+		this.owner = owner;
+	}
+	
+	
+	/**
+	 * Goes through all AI Entries in order and selects the best Action
+	 * @param state Current state of the game
+	 * @return The proposed Action
+	 */
+	public calculateBestAction( State state ) {
+	}
+
+
+
+
+}
---
+++ src/java/com/md/mechevo/game/State.java
@@ -0,0 +1,14 @@
+package com.md.mechevo.game;
+
+/**
+ * Holds all the information about the current state of game.
+ * Includes the map, units on field, teams, ...
+ */
+public class State {
+
+	private Map map;
+	private ArrayList<Team> teams;
+	
+	
+	public findPlayerById ( int id ) { }
+}
