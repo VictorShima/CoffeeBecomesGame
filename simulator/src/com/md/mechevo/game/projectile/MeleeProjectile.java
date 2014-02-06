413a29d12f8cd1623dfa03ee337ef04ee6b431b1
fdkio <fdkio@hotmail.com>
2014-02-06 18:04:54 +0000

    Resolved Imports

--- simulator/src/com/md/mechevo/game/projectile/MeleeProjectile.java
+++ simulator/src/com/md/mechevo/game/projectile/MeleeProjectile.java
@@ -1,8 +1,10 @@
 package com.md.mechevo.game.projectile;
 
+import com.md.mechevo.game.Obstacle;
 import com.md.mechevo.game.Player;
 import com.md.mechevo.game.Position;
 import com.md.mechevo.game.State;
+import com.md.mechevo.game.sentry.Sentry;
 import com.md.mechevo.game.weapon.Weapon;
 
 public class MeleeProjectile extends Projectile {
