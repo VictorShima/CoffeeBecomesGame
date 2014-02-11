a337622de3cdaaa18afeb5a459a1b67a4bb9cbde
fdkio <fdkio@hotmail.com>
2014-02-11 22:29:11 +0000

    Created more actions

---
+++ simulator/src/com/md/mechevo/game/action/Turn.java
@@ -0,0 +1,49 @@
+package com.md.mechevo.game.action;
+
+import com.md.mechevo.game.Player;
+import com.md.mechevo.game.State;
+
+public class Turn extends Action {
+    /**
+     * Duration of the action
+     *
+     * @todo Actual duration will depend on extra attribute
+     */
+    private static final int DURATION = 1000;
+
+
+    /**
+     * Whether or not the action is cancelable
+     */
+    private static final boolean CANCELABLE = true;
+
+
+    /**
+     * Class Constructor
+     */
+    public Turn(Player owner) {
+        super(owner, Turn.DURATION, Turn.CANCELABLE);
+    }
+
+
+    public boolean check(State state) {
+        return true; // always valid
+    }
+
+    @Override
+    public void begin(State state) {
+        getOwner().setMovementState(Player.MovementState.MOVING);
+        getOwner().setSpeed(Player.INITIAL_SPEED);
+    }
+
+
+    public void update(State state, float dtime) {
+        // TODO: do the actual moving
+    }
+
+
+    public void end(State state) { }
+
+
+}
+
